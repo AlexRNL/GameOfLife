7ce5e1820c61aaf59e4bbc7362eaca70784faffa
Alex Barféty <alexbarfety@free.fr>
2014-10-28 00:16:15 +0300

    Add WorldHistory class to compute world hashes

---
+++ src/main/java/com/alexrnl/gameoflife/service/WorldHistory.java
@@ -0,0 +1,77 @@
+package com.alexrnl.gameoflife.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.alexrnl.commons.error.ExceptionUtils;
+import com.alexrnl.gameoflife.world.World;
+
+/**
+ * Service class that maintain an history of the world.<br />
+ * To improve performances, only a hash of the world is internally stored.
+ * @author barfety_a
+ */
+public class WorldHistory {
+	/** Logger */
+	private static final Logger	LG					= Logger.getLogger(WorldHistory.class.getName());
+	
+	/** Number which indicate that the world has not been previously generated */
+	public static final int		NO_PREVIOUS_OCCURENCE	= -1;
+	
+	/** The algorithm to use for building the hashes. */
+	private final MessageDigest	hashAlgorithm;
+	/** The history of the world */
+	private final List<String>	history;
+	
+	/**
+	 * Constructor #1.<br />
+	 * @param hashAlgorithm
+	 *        the algorithm for the hash.
+	 */
+	public WorldHistory (final MessageDigest hashAlgorithm) {
+		super();
+		this.hashAlgorithm = hashAlgorithm;
+		this.history = new ArrayList<>();
+	}
+	
+	/**
+	 * Add a new world to the history.
+	 * @param world
+	 *        the world to add.
+	 * @return <code>true</code> if the world has already been generated.
+	 */
+	public int addHistory (final World world) {
+		final String hash = computeHash(world);
+		
+		if (history.contains(hash)) {
+			return history.indexOf(hash);
+		}
+		history.add(hash);
+		return NO_PREVIOUS_OCCURENCE;
+	}
+	
+	/**
+	 * Computes the hash of the world.
+	 * @param world
+	 *        the world to use.
+	 * @return the hash, as a string.
+	 */
+	private String computeHash (final World world) {
+		hashAlgorithm.reset();
+		
+		final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		try (final ObjectOutputStream out = new ObjectOutputStream(byteArrayOutputStream)) {
+			out.writeObject(world);
+			hashAlgorithm.update(byteArrayOutputStream.toByteArray());
+		} catch (final IOException e) {
+			LG.warning("Could not compute hash of world: " + ExceptionUtils.display(e));
+		}
+		
+		return new String(hashAlgorithm.digest());
+	}
+}
---
+++ src/main/java/com/alexrnl/gameoflife/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The package for the services of the game of life.
+ * @author barfety_a
+ */
+package com.alexrnl.gameoflife.service;
