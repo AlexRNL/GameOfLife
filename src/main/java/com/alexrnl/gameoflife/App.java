8dfd216e5fdd5e7962a282b652234db421105839
Alex Barféty <alexbarfety@free.fr>
2014-12-06 23:48:10 +0100

    Add basic display of world evolution

--- src/main/java/com/alexrnl/gameoflife/App.java
+++ src/main/java/com/alexrnl/gameoflife/App.java
@@ -1,13 +1,25 @@
 package com.alexrnl.gameoflife;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.alexrnl.commons.arguments.Arguments;
 import com.alexrnl.commons.arguments.Param;
 import com.alexrnl.commons.error.ExceptionUtils;
+import com.alexrnl.gameoflife.service.GameOfLifeController;
+import com.alexrnl.gameoflife.service.WorldFactory;
+import com.alexrnl.gameoflife.service.WorldListener;
+import com.alexrnl.gameoflife.world.Cell;
+import com.alexrnl.gameoflife.world.Coordinates;
+import com.alexrnl.gameoflife.world.World;
 
 /**
  * Class which launches the game of life.
@@ -22,11 +34,17 @@ public class App {
 	/** The default value for the alive ratio cell */
 	private static final double	DEFAULT_ALIVE_RATIO	= 0.3;
 	
+	/** The random generator to use */
+	private final Random		random;
+	/** Flag indicating to leave the generation */
+	private boolean				leave;
+	
+	// Command line parameters
 	/** The width of the world */
-	@Param(names = { "-w" }, description = "the width of the world", required = true)
+	@Param(names = { "-x" }, description = "the width of the world", required = true)
 	private int					width;
 	/** The height of the world */
-	@Param(names = { "-h" }, description = "the height of the world", required = true)
+	@Param(names = { "-y" }, description = "the height of the world", required = true)
 	private int					height;
 	/** The ratio of alive cells in the initial state */
 	@Param(names = { "-r" }, description = "the ratio of alive cell in the initial state")
@@ -41,6 +59,9 @@ public class App {
 	 */
 	public App (final List<String> arguments) throws IllegalArgumentException {
 		super();
+		random = new Random();
+		leave = false;
+		
 		aliveRatio = DEFAULT_ALIVE_RATIO;
 		new Arguments(PROGRAM_NAME, this).parse(arguments);
 		
@@ -64,6 +85,57 @@ public class App {
 			LG.info("Launching " + PROGRAM_NAME);
 		}
 		
+		final GameOfLifeController controller;
+		try {
+			controller = new GameOfLifeController(new WorldFactory(random).generate(width, height, aliveRatio));
+		} catch (final NoSuchAlgorithmException e) {
+			LG.severe("Alorithm for hashes do not exists: " + ExceptionUtils.display(e));
+			return;
+		}
+		controller.addWorldListener(new WorldListener() {
+			@Override
+			public void newGeneration (final World world) {
+				final SortedMap<Coordinates, Cell> sortedWorld = new TreeMap<>(new Comparator<Coordinates>() {
+					@Override
+					public int compare (final Coordinates o1, final Coordinates o2) {
+						final int ordinateComparison = o1.getY().compareTo(o2.getY());
+						if (ordinateComparison != 0) {
+							return ordinateComparison;
+						}
+						return o1.getX().compareTo(o2.getX());
+					}
+				});
+				for (final Entry<Coordinates, Cell> entry : world) {
+					sortedWorld.put(entry.getKey(), entry.getValue());
+				}
+				
+				final StringBuilder worldString = new StringBuilder();
+				int lastRow = 1;
+				for (final Entry<Coordinates, Cell> entry : sortedWorld.entrySet()) {
+					if (lastRow != entry.getKey().getY()) {
+						worldString.append(System.lineSeparator());
+					}
+					lastRow = entry.getKey().getY();
+					worldString.append(entry.getValue().isDead() ? ' ' : 'x');
+				}
+				System.out.println(worldString.toString());
+			}
+			
+			@Override
+			public void loopGeneration (final World world, final int previousOccurence) {
+				newGeneration(world);
+				System.out.println("Loop detected, previous occurence in generation " + previousOccurence);
+				leave = true;
+			}
+		});
+		
+		while (!leave) {
+			controller.grow();
+			try {
+				Thread.sleep(50);
+			} catch (final InterruptedException e) {}
+		}
+		
 		if (LG.isLoggable(Level.INFO)) {
 			LG.info("Exiting " + PROGRAM_NAME);
 		}
@@ -78,8 +150,7 @@ public class App {
 		try {
 			new App(Arrays.asList(args)).launch();
 		} catch (final IllegalArgumentException e) {
-			System.err
-					.println("Could not start " + PROGRAM_NAME + ": " + ExceptionUtils.display(e));
+			System.err.println("Could not start " + PROGRAM_NAME + ": " + ExceptionUtils.display(e));
 			e.printStackTrace();
 		}
 	}
